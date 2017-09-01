7bcd9b4c9605fe7b357668e52d3fd556b8cddd29
raffaeler <raffaeler@vevy.com>
2017-09-01 18:09:57 +0200

    Initial commit

---
+++ VanillaSolution/vanilla/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace vanilla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                $"Machine:     {Environment.MachineName}\r\n" +
+                $"OS         : { GetOS()}\r\n" +
+                $"Description: { RuntimeInformation.OSDescription}"
+                );
+
+        }
+
+        private static string GetOS()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return "Windows";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return "Linux";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) return "OSX";
+            return "Unknown";
+        }
+    }
+}
