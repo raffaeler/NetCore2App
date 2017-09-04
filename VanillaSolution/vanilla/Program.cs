393a0f222f798876117172d59a7d4d33bc341c21
raffaeler <raffaeler@vevy.com>
2017-09-04 11:11:07 +0200

    Simplified the csproj Added comments for publishing

--- VanillaSolution/vanilla/Program.cs
+++ VanillaSolution/vanilla/Program.cs
@@ -1,6 +1,24 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
+// In order to publish the application you have two choices:
+// Option 1: the target machine has the runtime installed
+//    dotnet publish -c Release -r linux-arm --self-contained=false
+// 
+//    -c Release     ==> specify to publish the release version (not the debug)
+//    -r linux-arm   ==> this tells the CLI to publish the required linux for ARM binaries
+//    --self-contained=false  ==> false can only be used when the runtime is available on the target machine
+//
+//
+// Option 2: the target machine may have the runtime or not
+//    dotnet publish -c Release -r linux-arm --self-contained
+//
+//    --self-contained ==> the implicit true is used to publish the whole set of binaries including the CLR
+//
+// With Option 1, you will need to run the app with the command: "dotnet vanilla.dll"
+// With Option 2, you will need to give the eXecutable permission: "chmod +x vanilla"
+//                and then you can run the app by executing it:  "./vanilla"
+
 namespace vanilla
 {
     class Program
